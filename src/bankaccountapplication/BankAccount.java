212d22648856619f66cd89c74117bda3a908e6e4
Isobella <isobella.sayers@csuglobal.edu>
2024-11-19 01:25:53 -0700

    Template Files to Work With, Attempted Basic Setup for Project

--- src/bankaccountapplication/BankAccount.java
+++ src/bankaccountapplication/BankAccount.java
@@ -1,13 +1,97 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package bankaccountapplication;
 
-/**
- *
- * @author isaye
- */
 public class BankAccount {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private Double balance;
     
+// Set initial Account Value to 0
+    public BankAccount() {
+        balance = 0.00;
+    }
+    
+// Setters and Getters
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public int getAccountID() {
+        return accountID;
+
+    }
+    
+    public double getBalance() {
+        return balance;
+
+    }
+    
+    // Methods
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdrawal(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+    
+
+    public void accountSummary() {
+        System.out.println("Account Summary:");
+        System.out.println("Username: " + username);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Account ID: " + accountID);
+        System.out.println("Balance: $" + balance);
+
+    }
+ @Override
+    public String toString() {
+        return String.format("%d, %s, %s, %s, %s, %f", getAccountID(), getUserName(),getFirstName(), getLastName(), getPassword(), getBalance()); // Hash password before saving
+  }
 }
+
+
+
+
--- src/bankaccountapplication/BankAccountApplication.java
+++ src/bankaccountapplication/BankAccountApplication.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bankaccountapplication;
 
-/**
- *
- * @author isaye
- */
+import java.awt.EventQueue;
+
+
 public class BankAccountApplication {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        EventQueue.invokeLater(() -> {
+        new EntryScreen().setVisible(true);
+   });
+        
     
+    
+}
 }
